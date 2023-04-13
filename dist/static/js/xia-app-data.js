c56e1db3ac2c7400c15d1cb60e665964e5ac005d
X-I-A <develop@x-i-a.com>
2023-04-13 20:32:31 +0200

    feat: first release

--- dist/static/js/xia-app-data.js
+++ dist/static/js/xia-app-data.js
@@ -23,6 +23,7 @@ class XiaAppData {
             return JSON.parse(atob(payloadBase64));
         } catch (error) {
             console.error('Error while decoding and parsing JWT payload:', error);
+            return {};
         }
     }
 
@@ -30,7 +31,7 @@ class XiaAppData {
         this.access_cookie = Cookies.get(this.accessCookieName);
         this.refresh_signal = Cookies.get(this.refreshSignalName);
         this.refresh_signal_body = this.getUnverifiedBody(this.refresh_signal);
-        if (!this.access_cookie && this.refresh_signal_body) {
+        if (!this.access_cookie && this.refresh_signal_body && ('token_info' in this.refresh_signal_body)) {
             const newLocation = this.refresh_signal_body['token_info']['root'];
             console.log("Refresh access token only for the same site")
             if (newLocation.startsWith("/")) {
